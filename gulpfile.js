8c23782ba4081772846fce1e75060ba98247be66
Harry Ronchetti <harry@ne6.digital>
2020-07-18 12:22:29 +0100

    gulp tasks improved, footer finished

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@
  * ----------------------------------------------------------------------
  */
 
-const srcPath = './src/';
-const staticPath = './static/';
-const distPath = './dist/';
+const srcPath = "./src/"
+const staticPath = "./static/"
+const distPath = "./dist/"
 
 /**
  * ----------------------------------------------------------------------
@@ -14,12 +14,13 @@ const distPath = './dist/';
  * ----------------------------------------------------------------------
  */
 
-let { src, dest, watch, series } = require('gulp');
-let browserSync = require('browser-sync');
-let rename = require('gulp-rename');
-let sass = require('gulp-sass');
-let uglify = require('gulp-uglify');
-let fileinclude = require('gulp-file-include');
+let { src, dest, watch, series } = require("gulp")
+let browserSync = require("browser-sync")
+let rename = require("gulp-rename")
+let sass = require("gulp-sass")
+let uglify = require("gulp-uglify")
+let fileinclude = require("gulp-file-include")
+let del = require("del")
 
 /**
  * ----------------------------------------------------------------------
@@ -27,35 +28,38 @@ let fileinclude = require('gulp-file-include');
  * ----------------------------------------------------------------------
  */
 
-function buildScss() {
-  return src(srcPath + 'scss/**/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(distPath + 'css/'));
+const buildScss = () => {
+  return src(srcPath + "scss/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest(distPath + "css/"))
 }
 
-function buildJS() {
-  return src(srcPath + 'js/**/*.js')
+const buildJS = () => {
+  return src(srcPath + "js/**/*.js")
     .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(distPath + 'js/'));
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest(distPath + "js/"))
 }
 
-function buildHTML() {
-  return src(srcPath + 'html/**/*.html')
+const buildHTML = () => {
+  return src(srcPath + "html/**/*.html")
     .pipe(
       fileinclude({
-        prefix: '@@',
-        basepath: '@file',
+        prefix: "@@",
+        basepath: "@file",
       })
     )
-    .pipe(dest(distPath));
+    .pipe(dest(distPath))
 }
 
-function buildStatic() {
-  return src(staticPath + '**/*').pipe(dest(distPath));
+const buildStatic = () => {
+  return src(staticPath + "**/*").pipe(dest(distPath))
 }
 
+const cleanBuild = () => {
+  return del([distPath])
+}
 /**
  * ----------------------------------------------------------------------
  *  Define exports/tasks
@@ -63,19 +67,22 @@ function buildStatic() {
  */
 
 exports.default = () => {
-  browserSync.init(['*.css', '*.scss'], {
+  browserSync.init(["*.css", "*.scss"], {
     server: {
-      baseDir: './dist',
+      baseDir: "./dist",
     },
-  });
-  watch(srcPath + 'js/**/*.js', buildJS);
-  watch(srcPath + 'scss/**/*.scss', buildScss);
-  watch(srcPath + 'html/**/*.html', buildHTML);
-  watch(staticPath + '**/*', buildStatic);
-  watch([srcPath + 'html/**/*.html', srcPath + 'js/**/*.js', srcPath + 'scss/**/*.scss']).on(
-    'change',
-    browserSync.reload
-  );
-};
+  })
+  watch(srcPath + "js/**/*.js", buildJS)
+  watch(srcPath + "scss/**/*.scss", buildScss)
+  watch(srcPath + "html/**/*.html", buildHTML)
+  watch(staticPath + "**/*", buildStatic)
+  watch([
+    srcPath + "html/**/*.html",
+    srcPath + "js/**/*.js",
+    srcPath + "scss/**/*.scss",
+  ]).on("change", browserSync.reload)
+}
 
-exports.build = series([buildJS, buildScss, buildHTML, buildStatic]);
+exports.build = series([cleanBuild, buildJS, buildScss, buildHTML, buildStatic])
+
+exports.clean = series(cleanBuild)
