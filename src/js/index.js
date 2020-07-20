7c254fe9f5808ba198d5528ec3160dec70ac03ba
Harry Ronchetti <harry@ne6.digital>
2020-07-20 15:31:22 +0100

    basket drawer minus zapiet finished

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,33 @@ const getFooterYear = () => {
 }
 
 document.addEventListener("DOMContentLoaded", getFooterYear)
-// Nav handlers
 
 const showMobileNav = () => {
   document.getElementById("nav-mobile").classList.add("active")
+  lockBodyScroll()
 }
 
 const hideMobileNav = () => {
   document.getElementById("nav-mobile").classList.remove("active")
+  unlockBodyScroll()
+}
+
+const showBasket = () => {
+  document.getElementById("basket-drawer").classList.add("active")
+  document.getElementById("basket-overlay").classList.add("active")
+  lockBodyScroll()
+}
+
+const hideBasket = () => {
+  document.getElementById("basket-drawer").classList.remove("active")
+  document.getElementById("basket-overlay").classList.remove("active")
+  unlockBodyScroll()
+}
+
+const lockBodyScroll = () => {
+  document.querySelector("body").classList.add("body-locked")
+}
+
+const unlockBodyScroll = () => {
+  document.querySelector("body").classList.remove("body-locked")
 }
