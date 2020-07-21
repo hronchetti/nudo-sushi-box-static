9b3f1edc6722cbb5d6b672e48eff4af1291996d3
Harry Ronchetti <harry@ne6.digital>
2020-07-21 21:58:04 +0100

    accounts finished

--- src/js/accounts-new-address.js
+++
@@ -1,9 +0,0 @@
-const showNewAddressForm = () => {
-  document.getElementById("accounts-new-address-form").classList.add("active")
-}
-
-const hideNewAddressForm = () => {
-  document
-    .getElementById("accounts-new-address-form")
-    .classList.remove("active")
-}
