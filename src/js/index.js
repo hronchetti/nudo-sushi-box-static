177ed68635d1b7d253b6696855018561a6412f69
Harry Ronchetti <harry@ne6.digital>
2020-07-23 14:49:28 +0100

    nav improved, no overflow

--- src/js/index.js
+++ src/js/index.js
@@ -11,31 +11,45 @@ document.addEventListener("DOMContentLoaded", () => {
 const showMobileNav = () => {
   document.getElementById("nav-mobile").classList.add("active")
   document.getElementById("nav-mobile-overlay").classList.add("active")
-  lockBodyScroll()
+  lockBodyScroll("nav-wrapper")
+  revealNavOverflow()
 }
 
 const hideMobileNav = () => {
   document.getElementById("nav-mobile").classList.remove("active")
   document.getElementById("nav-mobile-overlay").classList.remove("active")
-  unlockBodyScroll()
+  unlockBodyScroll("nav-wrapper")
+  setTimeout(hideNavOverflow, 500) // 500 milliseconds  matches CSS transition duration
 }
 
 const showBasket = () => {
   document.getElementById("basket-drawer").classList.add("active")
   document.getElementById("basket-overlay").classList.add("active")
-  lockBodyScroll()
+  lockBodyScroll("nav-wrapper")
+  revealNavOverflow()
 }
 
 const hideBasket = () => {
   document.getElementById("basket-drawer").classList.remove("active")
   document.getElementById("basket-overlay").classList.remove("active")
-  unlockBodyScroll()
+  unlockBodyScroll("nav-wrapper")
+  setTimeout(hideNavOverflow, 500) // 500 milliseconds  matches CSS transition duration
 }
 
-const lockBodyScroll = () => {
-  document.querySelector("body").classList.add("body-locked")
+const lockBodyScroll = (targetElementId) => {
+  // const targetElement = document.getElementById(targetElementId)
+  // bodyScrollLock.disableBodyScroll(targetElement)
 }
 
 const unlockBodyScroll = () => {
-  document.querySelector("body").classList.remove("body-locked")
+  // const targetElement = document.getElementById(targetElementId)
+  // bodyScrollLock.enableBodyScroll(targetElement)
+}
+
+const revealNavOverflow = () => {
+  document.getElementById("nav-wrapper").classList.add("active")
+}
+
+const hideNavOverflow = () => {
+  document.getElementById("nav-wrapper").classList.remove("active")
 }
